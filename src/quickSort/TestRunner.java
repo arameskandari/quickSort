dc522afad2c6ee65795657c6a5d878bdc7defc55
Aram Eskandari <KirmMacbookPro@n141-p191.eduroam.kth.se>
2018-02-07 14:45:31 +0100

    First commit

---
+++ src/quickSort/TestRunner.java
@@ -0,0 +1,20 @@
+package quickSort;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import primeNumber.PrimeNumberTest;
+
+public class TestRunner {
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(QuickSortTest.class);
+
+		for (Failure failure : result.getFailures()) {
+			System.out.println(failure.toString());
+			System.out.println("fail");
+		}
+
+		System.out.println(result.wasSuccessful());
+	}
+}
